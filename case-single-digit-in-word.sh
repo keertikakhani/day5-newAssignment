8e1dc66f75e948296214f22be925864f0e358a50
Your Name <you@example.com>
2022-11-11 15:36:00 +0530

    Added : case-single-digit-in-word in case-problems

---
+++ case-single-digit-in-word.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+read -p "enter the number : " a;
+
+case $a in
+	0)
+	echo "zero";
+	;;
+	1)
+	echo "One";
+	;;
+ 	2)
+	echo "Two";
+	;;
+	3)
+	echo "Three";
+	;;
+	4)
+	echo "Four";
+	;;
+	5)
+	echo "Five";
+	;;
+	6)
+	echo "Six";
+	;;
+	7)
+	echo "seven";
+	;;
+	8) 
+	echo "Eight";
+	;;
+	9)
+	echo "Nine";
+	;;
+esac
